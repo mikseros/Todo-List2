c188f81bd79678fd97613df1c485e4eb788fd1fa
mikseros <mmadynski@gmail.com>
2022-02-07 14:38:27 +0100

    Vue Layout Scaffold Created

---
+++ resources/js/components/app.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card-body">
+        Hello.
+     </div>     
+</template>
+
+<script>
+    export default {
+        
+        // mounted() {
+        //     console.log('Component mounted.')
+        // }
+    }
+</script>
